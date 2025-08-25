a4e9dccd08f5be40129cd902ce5e131846328ec8
sr-roboto <marcoslopez123.lu@gmail.com>
2025-08-25 07:19:58 -0300

    feat: :sparkles: Eliminar AppController y AppService, y agregar CarsController y CarsModule

---
+++ Seccion_02-Primeros_pasos_en_Nest/car-dealership/src/cars/cars.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Get, Param } from '@nestjs/common';
+
+@Controller('cars')
+export class CarsController {
+  private cars = ['Toyota', 'Honda', 'Ford'];
+
+  @Get()
+  getAllCars() {
+    return this.cars;
+  }
+
+  @Get(':id')
+  getCarById(@Param('id') id: string) {
+    return this.cars[id];
+  }
+}
