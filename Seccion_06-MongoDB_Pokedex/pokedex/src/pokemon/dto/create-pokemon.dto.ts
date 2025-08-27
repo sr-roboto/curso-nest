9711fe0cb86a0be5e14cfeca4a1d8b6b057b9593
sr-roboto <marcoslopez123.lu@gmail.com>
2025-08-27 12:04:14 -0300

    feat: :sparkles: Añadir validaciones y transformaciones para el DTO de Pokemon

--- Seccion_06-MongoDB_Pokedex/pokedex/src/pokemon/dto/create-pokemon.dto.ts
+++ Seccion_06-MongoDB_Pokedex/pokedex/src/pokemon/dto/create-pokemon.dto.ts
@@ -1,1 +1,12 @@
-export class CreatePokemonDto {}
+import { IsInt, IsPositive, IsString, Min, MinLength } from 'class-validator';
+
+export class CreatePokemonDto {
+  @IsString()
+  @MinLength(1)
+  readonly name: string;
+
+  @IsInt()
+  @IsPositive()
+  @Min(1)
+  readonly no: number;
+}
