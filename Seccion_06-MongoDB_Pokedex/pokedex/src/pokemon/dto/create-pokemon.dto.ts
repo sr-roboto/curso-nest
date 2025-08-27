3e9ca873d6a7ff47e44278324208066cb73fe75f
sr-roboto <marcoslopez123.lu@gmail.com>
2025-08-27 12:41:06 -0300

    feat: :sparkles: Modificar el DTO de Pokemon para eliminar la propiedad readonly y mejorar el manejo de errores en el servicio de Pokemon

--- Seccion_06-MongoDB_Pokedex/pokedex/src/pokemon/dto/create-pokemon.dto.ts
+++ Seccion_06-MongoDB_Pokedex/pokedex/src/pokemon/dto/create-pokemon.dto.ts
@@ -3,10 +3,10 @@ import { IsInt, IsPositive, IsString, Min, MinLength } from 'class-validator';
 export class CreatePokemonDto {
   @IsString()
   @MinLength(1)
-  readonly name: string;
+  name: string;
 
   @IsInt()
   @IsPositive()
   @Min(1)
-  readonly no: number;
+  no: number;
 }
